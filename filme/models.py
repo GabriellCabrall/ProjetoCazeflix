e5e37ae0a6bcc52b492b0df82977e0a81d6a59b5
Gabriel da Silva Cabral <98562652+GabriellCabrall@users.noreply.github.com>
2023-05-10 10:47:20 -0300

    Envia filme para o banco de dados

--- filme/models.py
+++ filme/models.py
@@ -1,8 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
+LISTA_CATEGORIAS = (
+    ('ANALISES', 'Análises'),
+    ('PROGRAMACAO', 'Programação'),
+    ('APRESENTACAO', 'Apresentação'),
+    ('OUTROS', 'Outros'),
+)
+
 # Criar o filme
+class Filme(models.Model):
+    titulo = models.CharField(max_length=200)
+    thumb = models.ImageField(upload_to='thumb_filmes')
+    descricao = models.TextField(max_length=10000)
+    categoria = models.CharField(max_length=20, choices=LISTA_CATEGORIAS)
+    visualizacoes = models.IntegerField(default=0)
+    data_criacao = models.DateTimeField(default=timezone.now)
+
 
 # Criar os episódios
 
