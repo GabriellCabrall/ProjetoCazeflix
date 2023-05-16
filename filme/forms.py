e83f1c376209b7e60fd9691ffe94c2fa35c7a996
Gabriel da Silva Cabral <98562652+GabriellCabrall@users.noreply.github.com>
2023-05-16 15:16:24 -0300

    Criando redirecionamento dinâmico na homepage

--- filme/forms.py
+++ filme/forms.py
@@ -3,6 +3,9 @@ from .models import Usuario
 from django import forms
 
 
+class FormHomepage(forms.Form):
+    email = forms.EmailField(label=False)
+
 class CriarContaForm(UserCreationForm):
     email = forms.EmailField()
 
--- filme/views.py
+++ filme/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect, reverse
-from .models import Filme
-from .forms import CriarContaForm
+from .models import Filme, Usuario
+from .forms import CriarContaForm, FormHomepage
 from django.views.generic import TemplateView, ListView, DetailView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
 
-class Homepage(TemplateView):
+class Homepage(FormView):
     template_name = 'homepage.html'
+    form_class = FormHomepage
 
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
@@ -16,6 +17,14 @@ class Homepage(TemplateView):
             return redirect('filme:homefilmes')
         else:
             return super().get(request, *args, **kwargs) # Redireciona para a homepage
+
+    def get_success_url(self):
+        email = self.request.POST.get('email')
+        usuarios = Usuario.objects.filter(email=email)
+        if usuarios:
+            return reverse('filme:login')
+        else:
+            return reverse('filme:criarconta')
 
 
 class Homefilmes(LoginRequiredMixin, ListView):
